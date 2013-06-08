efea4791c8bcf86082f588172b64390724b803d7
Sam Deane <sam@elegantchaos.com>
2013-06-08 12:54:10 +0100

    Expanding template project.

---
+++ ECCommandLineTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ECCommandLineTest
+//
+//  Created by Sam Deane on 08/06/2013.
+//  Copyright (c) 2013 Elegant Chaos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
